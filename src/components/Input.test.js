5cc8688836a1e93c69ca3f37819dc667df6f3d16
Jareth <seraphimjester@gmail.com>
2017-10-02 16:41:24 +0100

    Fix written better tests for Input

--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Renderer from 'react-test-renderer';
+import Enzyme, {shallow, mount, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+Enzyme.configure({ adapter: new Adapter() })
 import {Input} from './';
 
 describe('Input Field',()=>{
@@ -16,26 +19,43 @@ describe('Input Field',()=>{
     expect(Renderer.create(<Input icon="gear" />)).toMatchSnapshot();
   });
   it('accepts an onUpdate function',()=>{
-    expect(Renderer.create(<Input onUpdate= {val=>{console.log(val)}} />)).toMatchSnapshot();
+    //Init
+    const mockCallback = jest.fn();
+    let tree = mount(<Input onUpdate= {mockCallback} />)
+    //Alter
+    tree.find('input').simulate('change',{target:{value:"H"}})
+    //Check
+    expect(mockCallback.mock.calls).toMatchSnapshot();
   });
   it('rejects onUpdate non-function',()=>{
-    fail('not implemented')
+    let tree = mount(<Input onUpdate="Hello" />)
+    expect(()=>tree.find('input').simulate('change',{target:{value:"H"}})).toThrow(new Error('onUpdate from Input is not a function'));
   })
-  it('can error',()=>{
-    expect(Renderer.create(<Input error />)).toMatchSnapshot();
-    expect(Renderer.create(<Input error={true} />)).toMatchSnapshot();
+  it('correctly has error display',()=>{
+    expect(shallow(<Input />).find('input').hasClass('error')).toMatchSnapshot();
+    expect(shallow(<Input error={false} />).find('input').hasClass('error')).toMatchSnapshot();
+    expect(shallow(<Input error />).find('input').hasClass('error')).toMatchSnapshot();
+    expect(shallow(<Input error={true} />).find('input').hasClass('error')).toMatchSnapshot();
   });
-  it('can success',()=>{
-    expect(Renderer.create(<Input success />)).toMatchSnapshot();
-    expect(Renderer.create(<Input success={true} />)).toMatchSnapshot();
+  it('correctly has success display',()=>{
+    expect(shallow(<Input />).find('input').hasClass('success')).toMatchSnapshot();
+    expect(shallow(<Input success={false} />).find('input').hasClass('success')).toMatchSnapshot();
+    expect(shallow(<Input success />).find('input').hasClass('success')).toMatchSnapshot();
+    expect(shallow(<Input success={true} />).find('input').hasClass('success')).toMatchSnapshot();
   });
-  it('can disable',()=>{
-    expect(Renderer.create(<Input disabled />)).toMatchSnapshot();
-    expect(Renderer.create(<Input disabled={true} />)).toMatchSnapshot();
+  it('correctly has disable display',()=>{
+    expect(shallow(<Input />).find('input').hasClass('disabled')).toMatchSnapshot();
+    expect(shallow(<Input />).find('input').props()['disabled']).toMatchSnapshot();
+    expect(shallow(<Input disabled={false} />).find('input').hasClass('dissabled')).toMatchSnapshot();
+    expect(shallow(<Input disabled={false} />).find('input').props()['disabled']).toMatchSnapshot();
+    expect(shallow(<Input disabled />).find('input').hasClass('disabled')).toMatchSnapshot();
+    expect(shallow(<Input disabled />).find('input').props()['disabled']).toMatchSnapshot();
+    expect(shallow(<Input disabled={true} />).find('input').hasClass('disabled')).toMatchSnapshot();
+    expect(shallow(<Input disabled={true} />).find('input').props()['disabled']).toMatchSnapshot();
   })
   it('can have type',()=>{
-    expect(Renderer.create(<Input type="text" />)).toMatchSnapshot();
-    expect(Renderer.create(<Input type="password" />)).toMatchSnapshot();
+    expect(shallow(<Input type="text" />).find('input').props()['type']).toMatchSnapshot();
+    expect(shallow(<Input type="password" />).find('input').props()['type']).toMatchSnapshot();
   })
 
 })
