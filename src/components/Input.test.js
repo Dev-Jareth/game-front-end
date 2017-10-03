4014a1ccbb76d5fdbbc8baba155ab2ff113a6a26
Jareth <seraphimjester@gmail.com>
2017-10-03 13:21:44 +0100

    Edit Input tests improvement. Less snapshots

--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -10,10 +10,10 @@ describe('Input Field',()=>{
     expect(Renderer.create(<Input />)).toMatchSnapshot();
   });
   it('has a placeholder',()=>{
-    expect(Renderer.create(<Input placeholder="placeholder" />)).toMatchSnapshot();
+    expect(shallow(<Input placeholder="fakePlaceholder" />).find('input').props()['placeholder']).toBe('fakePlaceholder');
   });
   it('has a value',()=>{
-    expect(Renderer.create(<Input value="value" />)).toMatchSnapshot();
+    expect(shallow(<Input value="value" />).find('input').props()['value']).toBe('value');
   });
   it('has an FA icon',()=>{
     expect(Renderer.create(<Input icon="gear" />)).toMatchSnapshot();
@@ -32,30 +32,30 @@ describe('Input Field',()=>{
     expect(()=>tree.find('input').simulate('change',{target:{value:"H"}})).toThrow(new Error('onUpdate from Input is not a function'));
   })
   it('correctly has error display',()=>{
-    expect(shallow(<Input />).find('input').hasClass('error')).toMatchSnapshot();
-    expect(shallow(<Input error={false} />).find('input').hasClass('error')).toMatchSnapshot();
-    expect(shallow(<Input error />).find('input').hasClass('error')).toMatchSnapshot();
-    expect(shallow(<Input error={true} />).find('input').hasClass('error')).toMatchSnapshot();
+    expect(shallow(<Input />).find('input').hasClass('error')).toBe(false);
+    expect(shallow(<Input error={false} />).find('input').hasClass('error')).toBe(false);
+    expect(shallow(<Input error />).find('input').hasClass('error')).toBe(true);
+    expect(shallow(<Input error={true} />).find('input').hasClass('error')).toBe(true);
   });
   it('correctly has success display',()=>{
-    expect(shallow(<Input />).find('input').hasClass('success')).toMatchSnapshot();
-    expect(shallow(<Input success={false} />).find('input').hasClass('success')).toMatchSnapshot();
-    expect(shallow(<Input success />).find('input').hasClass('success')).toMatchSnapshot();
-    expect(shallow(<Input success={true} />).find('input').hasClass('success')).toMatchSnapshot();
+    expect(shallow(<Input />).find('input').hasClass('success')).toBe(false);
+    expect(shallow(<Input success={false} />).find('input').hasClass('success')).toBe(false);
+    expect(shallow(<Input success />).find('input').hasClass('success')).toBe(true);
+    expect(shallow(<Input success={true} />).find('input').hasClass('success')).toBe(true);
   });
   it('correctly has disable display',()=>{
-    expect(shallow(<Input />).find('input').hasClass('disabled')).toMatchSnapshot();
-    expect(shallow(<Input />).find('input').props()['disabled']).toMatchSnapshot();
-    expect(shallow(<Input disabled={false} />).find('input').hasClass('dissabled')).toMatchSnapshot();
-    expect(shallow(<Input disabled={false} />).find('input').props()['disabled']).toMatchSnapshot();
-    expect(shallow(<Input disabled />).find('input').hasClass('disabled')).toMatchSnapshot();
-    expect(shallow(<Input disabled />).find('input').props()['disabled']).toMatchSnapshot();
-    expect(shallow(<Input disabled={true} />).find('input').hasClass('disabled')).toMatchSnapshot();
-    expect(shallow(<Input disabled={true} />).find('input').props()['disabled']).toMatchSnapshot();
+    expect(shallow(<Input />).find('input').hasClass('disabled')).toBe(false);
+    expect(shallow(<Input />).find('input').props()['disabled']).toBeFalsy();
+    expect(shallow(<Input disabled={false} />).find('input').hasClass('dissabled')).toBe(false);
+    expect(shallow(<Input disabled={false} />).find('input').props()['disabled']).toBeFalsy();
+    expect(shallow(<Input disabled />).find('input').hasClass('disabled')).toBe(true);
+    expect(shallow(<Input disabled />).find('input').props()['disabled']).toBe(true);
+    expect(shallow(<Input disabled={true} />).find('input').hasClass('disabled')).toBe(true);
+    expect(shallow(<Input disabled={true} />).find('input').props()['disabled']).toBe(true);
   })
   it('can have type',()=>{
-    expect(shallow(<Input type="text" />).find('input').props()['type']).toMatchSnapshot();
-    expect(shallow(<Input type="password" />).find('input').props()['type']).toMatchSnapshot();
+    expect(shallow(<Input type="text" />).find('input').props()['type']).toBe('text');
+    expect(shallow(<Input type="password" />).find('input').props()['type']).toBe('password');
   })
 
 })
