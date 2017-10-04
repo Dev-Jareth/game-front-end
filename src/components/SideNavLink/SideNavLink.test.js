46b207c2381a071fa21e2b9d3df9699287764feb
Jareth <seraphimjester@gmail.com>
2017-10-04 17:18:28 +0100

    Added tests for SideNavLink

---
+++ src/components/SideNavLink/SideNavLink.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import Enzyme, {shallow, mount, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+import {MemoryRouter as R} from 'react-router-dom';
+Enzyme.configure({ adapter: new Adapter() })
+import {SideNavLink} from './';
+
+describe('SideNavLink',()=>{
+    it('renders',()=>{
+        expect(shallow(<SideNavLink />)).toMatchSnapshot();
+    });
+    it('correctly accepts exact path',()=>{
+        expect(shallow(<SideNavLink />).find('NavLink').props()['exact']).toBeFalsy();
+        expect(shallow(<SideNavLink exact={false} />).find('NavLink').props()['exact']).toBeFalsy();
+        expect(shallow(<SideNavLink exact />).find('NavLink').props()['exact']).toBe(true);
+        expect(shallow(<SideNavLink exact={true} />).find('NavLink').props()['exact']).toBe(true);
+    })
+    it('displays text',()=>expect(shallow(<SideNavLink>Sample Text</SideNavLink>).find('.side-nav-link-text').text()).toBe('Sample Text'));
+    it('correctly has TO',()=>expect(shallow(<SideNavLink to="fakelink" />).find('NavLink').props()['to']).toBe('fakelink'));
+    it('displays FA icon',()=>expect(shallow(<SideNavLink icon="house" />).find('.side-nav-link-icon').length).toBe(1));
+})
