9cd5a00b700f5c832a10bfa9faa34f7d060aca4a
Jareth <seraphimjester@gmail.com>
2017-10-05 10:40:42 +0100

    Added SideNav check for links, and links object diagram

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -3,8 +3,11 @@ import './SideNav.css';
 import {SideNavLink} from '../'
 
 export class SideNav extends Component {
+    /**
+     * links[{to,icon,exact,content}]
+     */
     render() {
-        let sideNavLinks = this.props.links.map(link=><SideNavLink key={link.to} to={link.to} icon={link.icon} exact={link.exact}>{link.content}</SideNavLink>)
+        let sideNavLinks = this.props.links && this.props.links.constructor === Array?this.props.links.map(link=><SideNavLink key={link.to} to={link.to} icon={link.icon} exact={link.exact}>{link.content}</SideNavLink>):[];
         return (
             <nav className="side-nav">
                 <ul>
