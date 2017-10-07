e91790349f1186e7b1d5188da8dcd74ed40aafa4
Jareth <seraphimjester@gmail.com>
2017-10-07 17:39:06 +0100

    Added redux store promise middleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import {applyMiddleware, createStore} from 'redux'
 import {createLogger} from 'redux-logger';
+import promise from 'redux-promise-middleware';
 import reducers from './reducers';
 
-const middleware = applyMiddleware(createLogger());
+const middleware = applyMiddleware(createLogger(),promise());
 export default createStore(reducers,middleware);
