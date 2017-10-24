862344b518ec65e4ca4f7935198db40aebe6775d
Jareth <seraphimjester@gmail.com>
2017-10-24 15:05:24 +0100

    Add THREE.JS begin work of Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,46 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import * as THREE from "three";
 export class Home extends Component {
-    render() {
-        return (
-            <h1>Home</h1>
-        )
+  constructor() {
+    super();
+    this.scene = new THREE.Scene();
+    this.geometry = new THREE.BoxGeometry(200, 200, 200);
+    this.material = new THREE.MeshStandardMaterial({
+      color: 0xffa0a0
+    });
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    let light = new THREE.PointLight(0x404040);
+    light.position.z = 1000;
+    this.scene.add(this.mesh);
+    this.scene.add(light);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+  }
+
+  componentDidMount = () => {
+    this.container.appendChild(this.renderer.domElement);
+    this.animate();
+  };
+  animate = () => {
+    requestAnimationFrame(this.animate);
+    this.mesh.rotation.x += Math.PI * 0.01;
+    this.mesh.rotation.y += Math.PI * 0.01;
+    this.renderer.render(this.scene, this.camera);
+    //   this.render()
+  };
+
+  render() {
+    if (typeof window !== "undefined") {
+      this.camera.position.z = 1000;
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      return (
+        <div
+          style={{ width: "inherit", height: "inherit", position: "absolute" }}
+          ref={thisNode => (this.container = thisNode)}
+        />
+      );
+    } else {
+      return null;
     }
+  }
 }
