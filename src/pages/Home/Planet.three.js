fbfb70c808b471976d912470bc6ba60a2e663776
Jareth <seraphimjester@gmail.com>
2017-10-26 11:53:49 +0100

    Add scaling to Planet rendering (for big nmber crunching but reasonable render size)

--- src/pages/Home/Planet.three.js
+++ src/pages/Home/Planet.three.js
@@ -24,35 +24,37 @@ import * as THREE from "three";
  */
 const calculateOrbiterVelocity = (planetRadius, orbitRadius) => {
   let planetDensityCoefficient = 5.515;
-  let G = 6.67*Math.pow(10,-11);
+  let G = 6.67 * Math.pow(10, -11);
   let mass = planetDensityCoefficient * Math.pow(planetRadius, 3);
   let velocity = Math.sqrt(G * mass / Math.pow(orbitRadius, 2));
   let timePeriod = 2 * Math.PI / velocity;
-  console.log(timePeriod+"s")
+  console.log("Hours of orbit:", timePeriod / 3600);
   return velocity;
 };
+const scale = 0.1;
+const orbitScale = 0.1
+
 export default (radius = 100, satelliteRadius, orbitDistance = radius + satelliteRadius) => {
   let accuracy = 100;
-  let geometry = new THREE.SphereGeometry(radius, accuracy, accuracy);
+  let geometry = new THREE.SphereGeometry(radius * scale, accuracy, accuracy);
   let planet = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({ color: 0x118888 }));
   let group = new THREE.Group();
   group.add(planet);
   let animate = null;
   if (satelliteRadius) {
-    orbitDistance = orbitDistance + radius;
-    let geometry = new THREE.SphereGeometry(satelliteRadius, accuracy, accuracy);
+    let geometry = new THREE.SphereGeometry(satelliteRadius * scale, accuracy, accuracy);
     let satellite = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({ color: 0xf8f8f8 }));
     satellite.position.x = orbitDistance;
     group.add(satellite);
     let velocity = calculateOrbiterVelocity(radius, orbitDistance);
     animate = t => {
-      t = (t * velocity / (1)) % (2 * Math.PI);
+      t = (t * velocity ) % (2 * Math.PI);
       requestAnimationFrame(animate);
-      satellite.position.x = Math.sin(t) * orbitDistance;
-      satellite.position.y = Math.cos(t) * orbitDistance;
-      satellite.position.z = Math.cos(t) * orbitDistance;
+      satellite.position.x = Math.sin(t) * orbitDistance * scale * orbitScale;
+      satellite.position.y = Math.cos(t) * orbitDistance * scale* orbitScale;
+      satellite.position.z = Math.cos(t) * orbitDistance * scale* orbitScale;
     };
-    animate()
+    animate();
   }
   return group;
 };
