b856b737c321368d4f051346bed61c97c78d3dd3
Jareth <seraphimjester@gmail.com>
2017-10-26 11:28:30 +0100

    Fix Planet calculation accuracy. Fix Home index loader was BufferGeometryLoader. now JSONLoader. Materials now load Refactor Planet returns only object now (animation internal) Refactor Planet calculate velocity moved out of requestAnimationFrame Note: remember to set Blender export settings Note: Commit more often!!

--- src/pages/Home/Planet.three.js
+++ src/pages/Home/Planet.three.js
@@ -23,32 +23,36 @@ import * as THREE from "three";
  * t ∝ c^-1
  */
 const calculateOrbiterVelocity = (planetRadius, orbitRadius) => {
-  let planetDensityCoefficient = 1;
-  let G = 6.67;
+  let planetDensityCoefficient = 5.515;
+  let G = 6.67*Math.pow(10,-11);
   let mass = planetDensityCoefficient * Math.pow(planetRadius, 3);
   let velocity = Math.sqrt(G * mass / Math.pow(orbitRadius, 2));
   let timePeriod = 2 * Math.PI / velocity;
+  console.log(timePeriod+"s")
   return velocity;
 };
 export default (radius = 100, satelliteRadius, orbitDistance = radius + satelliteRadius) => {
   let accuracy = 100;
   let geometry = new THREE.SphereGeometry(radius, accuracy, accuracy);
-  let planet = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({ color: 0x888888 }));
+  let planet = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({ color: 0x118888 }));
   let group = new THREE.Group();
   group.add(planet);
   let animate = null;
   if (satelliteRadius) {
     orbitDistance = orbitDistance + radius;
     let geometry = new THREE.SphereGeometry(satelliteRadius, accuracy, accuracy);
-    let satellite = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({ color: 0x888888 }));
+    let satellite = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({ color: 0xf8f8f8 }));
     satellite.position.x = orbitDistance;
     group.add(satellite);
+    let velocity = calculateOrbiterVelocity(radius, orbitDistance);
     animate = t => {
-      t = (t * calculateOrbiterVelocity(radius, orbitDistance) / (1000 * 60)) % (2 * Math.PI);
+      t = (t * velocity / (1)) % (2 * Math.PI);
       requestAnimationFrame(animate);
       satellite.position.x = Math.sin(t) * orbitDistance;
       satellite.position.y = Math.cos(t) * orbitDistance;
+      satellite.position.z = Math.cos(t) * orbitDistance;
     };
+    animate()
   }
-  return { objects: group, animate };
+  return group;
 };
