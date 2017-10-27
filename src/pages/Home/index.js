169740f7e2bd58c94a4ad64969a9353b40776477
Jareth <seraphimjester@gmail.com>
2017-10-27 14:57:09 +0100

    Clean Home index ternary operators on controls + comments, formatted file

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,10 +12,13 @@ export class Home extends Component {
   constructor() {
     super();
     this.scene = new THREE.Scene();
-    this.player = new Player('ship',this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100000));
+    this.player = new Player(
+      "ship",
+      (this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100000))
+    );
     let light = new THREE.PointLight(0x404040);
     light.position.z = 1000;
-    let earth = new Planet(6371, 1737,384400);
+    let earth = new Planet(6371, 1737, 384400);
     earth.position.z = 1500;
     this.scene.add(earth);
     this.scene.add(light);
@@ -31,29 +34,33 @@ export class Home extends Component {
     let accelStrength = 5 / 100;
     let maxSpeed = 5;
 
-    if (w.pressed) w.rate = Math.min(maxSpeed, Math.max(1, w.rate + accelStrength));
-    else w.rate = Math.max(0, w.rate - damperStrength);
+    //W
+    w.pressed
+      ? (w.rate = Math.min(maxSpeed, Math.max(1, w.rate + accelStrength)))
+      : (w.rate = Math.max(0, w.rate - damperStrength));
     this.player.translateZ(w.rate);
-    if (s.pressed) s.rate = Math.min(maxSpeed, Math.max(1, s.rate + accelStrength));
-    else s.rate = Math.max(0, s.rate - damperStrength);
+    //S
+    s.pressed
+      ? (s.rate = Math.min(maxSpeed, Math.max(1, s.rate + accelStrength)))
+      : (s.rate = Math.max(0, s.rate - damperStrength));
     this.player.translateZ(-s.rate);
-    if (a.pressed) a.rate = 1;
-    else a.rate = Math.max(0, a.rate - damperStrength);
+    //A
+    a.pressed ? (a.rate = 1) : (a.rate = Math.max(0, a.rate - damperStrength));
     this.player.rotateZ(a.rate * -0.01);
-    if (d.pressed) d.rate = 1;
-    else d.rate = Math.max(0, d.rate - damperStrength);
+    //D
+    d.pressed ? (d.rate = 1) : (d.rate = Math.max(0, d.rate - damperStrength));
     this.player.rotateZ(d.rate * 0.01);
-    if (q.pressed) q.rate = 1;
-    else q.rate = Math.max(0, q.rate - damperStrength);
+    //Q
+    q.pressed ? (q.rate = 1) : (q.rate = Math.max(0, q.rate - damperStrength));
     this.player.rotateY(q.rate * 0.008);
-    if (e.pressed) e.rate = 1;
-    else e.rate = Math.max(0, e.rate - damperStrength);
+    //E
+    e.pressed ? (e.rate = 1) : (e.rate = Math.max(0, e.rate - damperStrength));
     this.player.rotateY(e.rate * -0.008);
-    if (space.pressed) space.rate = 1;
-    else space.rate = Math.max(0, space.rate - damperStrength);
+    //SPACE
+    space.pressed ? (space.rate = 1) : (space.rate = Math.max(0, space.rate - damperStrength));
     this.player.rotateX(space.rate * -0.01);
-    if (shift.pressed) shift.rate = 1;
-    else shift.rate = Math.max(0, shift.rate - damperStrength);
+    //SHIFT
+    shift.pressed ? (shift.rate = 1) : (shift.rate = Math.max(0, shift.rate - damperStrength));
     this.player.rotateX(shift.rate * 0.01);
   };
   _keyDown = e => {
