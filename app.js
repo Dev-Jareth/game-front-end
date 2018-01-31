2910f6236168bbac03d7f22a124649827654f7b1
Jareth <seraphimjester@gmail.com>
2018-01-31 13:59:17 +0000

    Setting up for JWT authentication

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var http = require('http');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 // view engine setup
@@ -18,12 +18,14 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('game', { title: 'Express' });
+  res.render('game', {
+    title: 'Play'
+  });
 });
+router.get('/login', (req, res, next) => {
+  console.log(req.query)
+  let error = req.query.error
+  res.render('login', {
+    error
+  })
+})
+router.post('/login', (req, res, next) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  if (email == "email" && password == "password")
+    res.redirect('/');
+  else res.redirect('/login?error=badCredentials')
+})
 
 module.exports = router;
