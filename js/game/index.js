6a0d663dccaea24e826519a75f8458c87c44f0f3
Jareth <seraphimjester@gmail.com>
2018-02-09 12:00:05 +0000

    Refactor clean index.js removed rotation on object[0] from animate function. moved code from run() to new init() condensed code sorted declarations on page. removed parameter and await on GUI init() call from run() commented out window.map declaration maybe something else too...

--- js/game/index.js
+++ js/game/index.js
@@ -3,35 +3,54 @@ import { Planet, StarCloud } from './models';
 import player, { camera, calculatePlayerMove, keyboard, keyboardListeners, playerGUI, setGUIRenderer } from './player';
 import { SCREEN_WIDTH, SCREEN_HEIGHT, kmToM, updateScreenResolution } from './util';
 import { map, loadJsonToMap } from './map';
-/*######Debug######*/
-window.map = map;
-/*#################*/
 import jsonData from './fakeData.json';
-const {_keyUp, _keyDown} = {
-  ...keyboardListeners
-};
+/*######Debug######*/
+// window.map = map;
+/*#################*/
+//Variables
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+renderer.autoClear = false;
+map.player.player = player;
+let mso = 0;
+//Functions
+const addEventListeners = () => {
+  document.addEventListener("keydown", keyboardListeners._keyDown);
+  document.addEventListener("keyup", keyboardListeners._keyUp);
+}
 const updateResolution = () => {
   updateScreenResolution()
   renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
   camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
   camera.updateProjectionMatrix();
 }
-const renderer = new THREE.WebGLRenderer({
-  antialias: true
-});
-renderer.autoClear = false;
-setGUIRenderer(renderer)
-const scene = new THREE.Scene();
-map.player.player = player;
-const addEventListeners = () => {
-  document.addEventListener("keydown", _keyDown);
-  document.addEventListener("keyup", _keyUp);
+const loadMap = () => {
+  map.objects.forEach(addObjToScene)
+  scene.add(map.player.player);
 }
-const run = async (gameContainer = document.getElementById('game-container')) => {
+const addObjToScene = obj => {
+  if (!scene.getObjectById(obj.uuid)) scene.add(obj);
+}
+const animate = ms => {
+  let delta = (ms - mso) / 1000
+  mso = ms;
+  //Move player
+  calculatePlayerMove(delta || 0);
+  //Update LOD objects to show correct detail
+  map.objects.forEach(obj => obj instanceof THREE.LOD ? obj.update(camera) : void (0))
+  //Draw & Re-call//
+  updateResolution()
+  requestAnimationFrame(animate);
+  renderer.clear()
+  renderer.render(scene, camera);
+  renderer.clearDepth()
+  playerGUI.update(ms)
+}
+const init = gameContainer => {
   gameContainer.classList.remove("hidden");
-  await playerGUI.init(document.getElementById('player-gui'));
-  let ambientLight = new THREE.AmbientLight(0x404040, 0.5);
-  let pointLight = new THREE.PointLight();
+  gameContainer.appendChild(renderer.domElement);
   let boundingBox = new THREE.Mesh(new THREE.BoxGeometry(map.x.max - map.x.min, map.y.max - map.y.min, map.z.max - map.z.min, 10, 10, 10), new THREE.MeshBasicMaterial({
     wireframe: true,
     color: 0x002e78
@@ -43,10 +62,9 @@ const run = async (gameContainer = document.getElementById('game-container')) =>
     transparent: true,
     opacity: 0.3
   })))
-  scene.add(ambientLight);
-  scene.add(pointLight);
-  scene.add(boundingBox);
-  scene.add(StarCloud({
+  scene.add(new THREE.AmbientLight(0x404040, 0.5));
+  scene.add(new THREE.PointLight());
+  scene.add(boundingBox);scene.add(StarCloud({
     density: 0.001,
     coords: {
       x: map.x,
@@ -54,35 +72,15 @@ const run = async (gameContainer = document.getElementById('game-container')) =>
       z: map.z
     }
   }))
-  gameContainer.appendChild(renderer.domElement);
-
+}
+const run = async (gameContainer = document.getElementById('game-container')) => {
+  init(gameContainer)
   loadJsonToMap(jsonData);
   loadMap();
-  animate();
   addEventListeners();
-}
-const loadMap = () => {
-  map.objects.forEach(addObjToScene)
-  scene.add(map.player.player);
-}
-const addObjToScene = obj => {
-  if (!scene.getObjectById(obj.uuid)) scene.add(obj);
-}
-let mso = 0;
-const animate = ms => {
-  let delta = (ms - mso) / 1000
-  mso = ms;
-  calculatePlayerMove(delta || 0);
-
-  map.objects.forEach(obj => obj instanceof THREE.LOD ? obj.update(camera) : void (0))
-  map.objects[0].rotation.y += 0.001;
-  //Draw & Re-call//
-  updateResolution()
-  requestAnimationFrame(animate);
-  renderer.clear()
-  renderer.render(scene, camera);
-  renderer.clearDepth()
-  playerGUI.update(ms)
+  setGUIRenderer(renderer)
+  playerGUI.init();
+  animate();
 }
 
 
