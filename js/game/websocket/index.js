3e0805b2ef270c7b0d02140637cc4039aa7e39cf
Jareth <seraphimjester@gmail.com>
2018-02-16 17:03:27 +0000

    Add socket message validation, Clean websocket index error check on subscriptions and message sends. Throws error if not valid type cleaned up file slightly

--- js/game/websocket/index.js
+++ js/game/websocket/index.js
@@ -4,25 +4,13 @@ import player from '../player';
 import { print, printErr } from '../util';
 import { baseSocketUrl, socket } from '../config';
 import config from '../../config';
+
+//Vars//
 const req = config.url.requestSocketMessageTypes;
 var ws;
-export const connect = async () => {
-  if (!socket.messages) await getMessageTypes()
-  ws = new WebSocket(baseSocketUrl);
-  ws.onerror = () => printErr('WebSocket error');
-  ws.onopen = () => print('WebSocket connection established');
-  ws.onclose = () => print('WebSocket connection closed');
-  ws.onmessage = handleMessage;
-}
-export const getMessageTypes = async () => {
-  const handle = f => (v => f(v.data));
-  await axios[req.method](req.path, { headers: { token: player.userData.authenticationKey } })
-  .then(handle(socket.setMessages))
-}
-export const sendMessage = (k, v = false) => ws.send(JSON.stringify({
-  [k]: v
-}))
 
+//Funcs//
+const publish = PubSub.publish;
 const handleMessage = message => {
   print("WS Message Received")
   try {
@@ -35,6 +23,23 @@ const handleMessage = message => {
     printErr("WS Failed to parse message")
   }
 }
-const publish = PubSub.publish;
-export const subscribe = (key, callback) => PubSub.subscribe(key, (a, b) => callback(b));
+const handleAxios = f => (v => f(v.data));
+const isMessage = (cs,msg)=>Boolean(Object.keys(socket.messages[cs]).find(key=>socket.messages[cs][key] == msg));
+const isServerMessage = msg=> isMessage('server',msg);
+const isClientMessage = msg=> isMessage('client',msg);
+const notMessage = (type, client) => {throw new Error(`${type} is not type of Socket ${client?"Client":"Server"} Message`)};
+
+//Exports//
+export const connect = async () => {
+  if (!socket.messages) await getMessageTypes()
+  ws = new WebSocket(baseSocketUrl);
+  ws.onerror = () => printErr('WebSocket error');
+  ws.onopen = () => print('WebSocket connection established');
+  ws.onclose = () => print('WebSocket connection closed');
+  ws.onmessage = handleMessage;
+}
+export const getMessageTypes = async () => await axios[req.method](req.path, { headers: { token: player.userData.authenticationKey } }).then(handleAxios(socket.setMessages))
+
+export const sendMessage = (k, v = false) => isClientMessage(k)?ws.send(JSON.stringify({ [k]: v })):notMessage(k,true);
+export const subscribe = (key, callback) => isServerMessage(key)? PubSub.subscribe(key, (a, b) => callback(b)) : notMessage(key,false);
 export const unsubscribe = PubSub.unsubscribe;
